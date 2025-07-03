6b44d5a90397d4b94842316f4db08133e360c4f7
Daniel McKnight <daniel@neon.ai>
2025-07-02 17:30:28 -0700

    Minor bugfixes

--- docker_overlay/opt/neon/healthcheck.sh
+++ docker_overlay/opt/neon/healthcheck.sh
@@ -25,7 +25,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-port="${HEALTHCHECK_PORT:-8080}"
+port="${HEALTHCHECK_PORT:-8000}"
 # Perform the health check using curl
 resp_content=$(curl -s http://localhost:${port}/status)
 status=$(echo "${resp_content}" | jq -r '.status')
--- neon_metrics_service/metrics_connector.py
+++ neon_metrics_service/metrics_connector.py
@@ -55,7 +55,7 @@ class NeonMetricsConnector(MQConnector):
         if not MQConnector.check_health(self):
             self.status.set_error("MQConnector health check failed")
             return False
-        return self.status.check_ready
+        return self.status.check_ready()
 
     def stop(self):
         self.status.set_stopping()
